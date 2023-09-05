3337d46ca13046bbe76e094218d9d3e234e4daf5
liangzsh <389878258@qq.com>
2023-09-05 18:25:33 +0800

    my
    修改部分ui

--- app/src/main/java/com/qiusuo/videoeditor/module/home/DraftFragment.kt
+++ app/src/main/java/com/qiusuo/videoeditor/module/home/DraftFragment.kt
@@ -4,28 +4,33 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
 import com.qiusuo.videoeditor.R
 import com.qiusuo.videoeditor.base.BaseFragment
 import com.qiusuo.videoeditor.databinding.FragmentDraftBinding
+import com.qiusuo.videoeditor.view.adapter.DraftAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-// TODO: Rename parameter arguments, choose names that match
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 class DraftFragment : BaseFragment<FragmentDraftBinding>(FragmentDraftBinding::inflate) {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var draftAdapter: DraftAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
     }
 
     override fun initView() {
+        lifecycleScope.launch {
+            withContext(Dispatchers.IO) {
+
+            }
+        }
+        draftAdapter = DraftAdapter()
+        viewBinding.rv.adapter = draftAdapter
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -39,8 +44,6 @@ class DraftFragment : BaseFragment<FragmentDraftBinding>(FragmentDraftBinding::i
         fun newInstance(param1: String, param2: String) =
             DraftFragment().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
             }
     }
---
+++ app/src/main/java/com/qiusuo/videoeditor/view/adapter/DraftAdapter.kt
@@ -0,0 +1,29 @@
+package com.qiusuo.videoeditor.view.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.qiusuo.videoeditor.common.bean.Project
+import com.qiusuo.videoeditor.databinding.ItemDraftLayoutBinding
+
+class DraftAdapter(val list: List<Project>) : RecyclerView.Adapter<DraftAdapter.DraftHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DraftHolder {
+        return DraftHolder(ItemDraftLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    override fun onBindViewHolder(holder: DraftHolder, position: Int) {
+        holder.binding
+    }
+
+    inner class DraftHolder(val binding: ItemDraftLayoutBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun onbind() {
+
+        }
+    }
+}
